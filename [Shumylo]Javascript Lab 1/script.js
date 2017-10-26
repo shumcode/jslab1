cadd42e8a920a30b76b8f88e159c512b7b521fae
shumcode <cshumylo@gmail.com>
2017-10-26 19:13:07 -0400

    finished

--- [Shumylo]Javascript Lab 1/script.js
+++ [Shumylo]Javascript Lab 1/script.js
@@ -2,26 +2,25 @@ var userHealth = 40;
 var grantHealth = 10;
 var wins = 0;
 var playing = true;
-
-var prompt0 = prompt("Would you like to play an RPG?", "yes or no");
+var prompt0 = prompt("Would you like to play an RPG?", "yes or no").toLowerCase();
 
 if (prompt0 === "yes") {
-var user = prompt("what is your name?");
-while (playing) {
-  userHealth -= Math.floor((Math.random() * 2) + 1);
-  grantHealth -= Math.floor((Math.random() * 2) + 1);
-  console.log(user + " has " + userHealth + " health left.");
-  console.log("Grant the mighty Chicken has " + grantHealth + " health left.");
-  if (grantHealth <= 0) {
-    grantHealth = 10;
-    wins++;
+  var user = prompt("what is your name?");
+  while (playing) {
+    userHealth -= Math.floor((Math.random() * 2) + 1);
+    grantHealth -= Math.floor((Math.random() * 2) + 1);
+    console.log(user + " has " + userHealth + " health left.");
+    console.log("Grant the mighty Chicken has " + grantHealth + " health left.");
+    if (grantHealth <= 0) {
+      grantHealth = 10;
+      wins++;
+    }
+    if (userHealth <= 0) {
+      console.log("The game is over.");
+      playing = false;
+    }
+    if (wins === 3) {
+      playing = false;
+    }
   }
-  if (userHealth <= 0) {
-    console.log("The game is over.");
-    playing = false;
-  }
-  if (wins === 3) {
-    playing = false;
-  }
-}
 }
