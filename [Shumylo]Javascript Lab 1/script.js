06fd6e59006050e79d3a7bf0cd53093b3a6e25c9
shumcode <cshumylo@gmail.com>
2017-10-26 22:17:56 -0400

    oops i forgot a few things

--- [Shumylo]Javascript Lab 1/script.js
+++ [Shumylo]Javascript Lab 1/script.js
@@ -10,16 +10,17 @@ if (prompt0 === "yes") {
     userHealth -= Math.floor((Math.random() * 2) + 1);
     grantHealth -= Math.floor((Math.random() * 2) + 1);
     console.log(user + " has " + userHealth + " health left.");
-    console.log("Grant the mighty Chicken has " + grantHealth + " health left.");
+    console.log("Grant the Mighty Chicken has " + grantHealth + " health left.");
     if (grantHealth <= 0) {
       grantHealth = 10;
       wins++;
     }
     if (userHealth <= 0) {
-      console.log("The game is over.");
+      console.log("The game is over. Grant the Mighty Chicken has won!");
       playing = false;
     }
     if (wins === 3) {
+      console.log(user + " has defeated Grant the Mighty Chicken with " + userHealth + " health left!");
       playing = false;
     }
   }
