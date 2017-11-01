16bd7419d036a2a30df3a3ff430432af1b6b7895
shumcode <cshumylo@gmail.com>
2017-10-31 20:21:46 -0400

    awesome

--- lab part 3/[Shumylo]Javascript Lab 1/script.js
+++ lab part 3/[Shumylo]Javascript Lab 1/script.js
@@ -2,7 +2,7 @@ var playing = true;
 var userChoice = null;
 
 var character = {
-  wins: 0,
+  wins: 5,
   health: 40,
   healsRemaining: 2,
   name: prompt("Enter your name!"),
@@ -13,8 +13,12 @@ var character = {
     if (this.healsRemaining === 0) {
       return;
     }
-    this.healsRemaining--;
+    if (this.health >= 40) {
+      console.log("Your health is at max!");
+      return;
+    }
     this.health += Math.floor((Math.random() * 10) + 1);
+    this.healsRemaining--;
   }
 }
 
@@ -27,42 +31,43 @@ var grant = {
 }
 
 if (character.name != "") {
-  startGame();
+  startCombat();
 }
 
-function startGame() {
+function startCombat() {
   while (playing) {
     var userChoice = prompt("Would you like to Attack, Heal, or Quit?").toLowerCase();
-    character.health -= character.generateAttackDamage();
-    console.log(character.name + " has " + character.health + " health left!");
+    if (userChoice === "heal") {
+      character.heal();
+      console.log("You have " + character.healsRemaining + " heal(s) remaining!");
+      console.log(grant.name + " has " + grant.health + " health left.");
+    }
     if (userChoice === "attack") {
       grant.health -= grant.generateAttackDamage();
       console.log(grant.name + " has " + grant.health + " health left.");
     }
-    if (userChoice === "heal") {
-      character.heal();
-      console.log(grant.name + " has " + grant.health + " health left.");
-      console.log("You have " + character.healsRemaining + " heal(s) remaining!");
-    }
     if (userChoice === "quit") {
-      console.log("The game is over. " + grant.name + " has prevailed!");
+      console.log("The game is over. " + grant.name + " has beaten you so bad that you quit like a baby!");
+      grant.health = 10;
       break;
     }
+    character.health -= character.generateAttackDamage();
+    console.log(character.name + " has " + character.health + " health left!");
     if (character.health <= 0) {
       console.log("The game is over. " + grant.name + " has won!");
       break;
     }
     if (grant.health <= 0) {
       grant.health = 10;
-      character.wins++;
+      character.wins--;
       winMessage();
     }
-    if (character.wins === 5) {
+    if (character.wins === 0) {
       console.log("You beat Grant Chirpus!");
+      grant.health = 10;
+      character.health = 40;
+      character.wins = 5;
       break;
-    }
-    if (character.healsRemaining === 0 && userChoice === "heal") {
-    
     }
   }
 }
@@ -70,114 +75,3 @@ function startGame() {
 function winMessage() {
   console.log("You have defeated " + grant.name + "!" +  " You need " + character.wins + " win(s) left!");
 }
-
-
-
-
-
-
-
-
-
-
-// startGame();
-
-
-// function startGame() {
-//   var nameEntry = prompt("Enter your name.").toLowerCase();
-//   if (nameEntry != "") {
-//     var attackDecision = prompt("Attack, Heal, or Quit!").toLowerCase();
-//   }
-// }
-
-// class Character {
-//   constructor(name, health, healsRemaining, wins) {
-//     this.name = name;
-//     this.health = health;
-//     this.healsRemaining = healsRemaining;
-//     this.wins = wins;
-//   }
-//   generateAttackDamage () {
-//     delete this.healsRemaining;
-//     delete this.wins;
-//     return Math.floor((Math.random() * 3) + 1);
-//   }
-//   heal() {
-//     var healingpoints = Math.floor((Math.random() * 10) + 1);
-//     this.health += healingpoints;
-//     this.healsRemaining - 1;
-//   }
-// }
-
-// var user = new Character(startGame(), 40, 2, null);
-// var grant = new Character("Grant Chirpus", 10, null, null);
-
-// grant.generateAttackDamage();
-
-// console.log(grant);
-// console.log(user);
-
-// function startGame() {
-//   prompt0 = prompt("Would you like to play an RPG?", "yes or no").toLowerCase();
-//   if (prompt0 === "yes") {
-//     user = prompt("What is your name?");
-//     if (user != false) {
-//       whichEnemy = prompt("Who do you want to fight?", "grant or ultraboss").toLowerCase();
-//     }
-//     if (whichEnemy === "ultraboss") {
-//       enemyHealth = 20;
-//       weapon = prompt("You'll need a weapon against the ULTRABOSS" , "sword, bow, staff").toLowerCase();
-//     }
-//     console.log("The battle begins!");
-//     startCombat();
-//   }
-// }
-
-// function startCombat() {
-//   while (playing) {
-//     quit = prompt("Would you like to Attack, Heal, or Quit?", "Attack, Heal, or Quit").toLowerCase();
-//     if (quit === "quit") {
-//       userHealth = 40;
-//       enemyHealth = 10;
-//       wins = 5;
-//       console.log("See you later, try again!");
-//       break;
-//     }
-//     if (quit === "heal") {
-//       character.healMe();
-//       console.log(user + " has " + userHealth + " health left.");
-//     }
-//     if (quit === "attack") {
-//       getDamage();
-//       console.log(user + " has " + userHealth + " health left.");
-//       if (whichEnemy === "grant") {
-//         console.log("Grant the Mighty Chicken has " + enemyHealth + " health left.");
-//       } else {
-//         console.log("THE ULTRABOSS has " + enemyHealth + " health left.");
-//       }
-//       if (enemyHealth <= 0) {
-//         if (whichEnemy === "ultraboss") {
-//           enemyHealth = 20;
-//         } else {
-//           enemyHealth = 10;
-//         }
-//         wins--;
-//         winMessage();
-//       }
-//       if (userHealth <= 0) {
-//         console.log("The game is over. " + whichEnemy + " has won!");
-//         userHealth = 40;
-//         enemyHealth = 10;
-//         wins = 5;
-//         break;
-//       }
-//       if (wins === 0) {
-//         console.log(user + " has defeated " + whichEnemy + " with " + userHealth + " health left!");
-//         userHealth = 40;
-//         enemyHealth = 10;
-//         wins = 5;
-//         break;
-//       }
-//     }
-//   }
-// }
